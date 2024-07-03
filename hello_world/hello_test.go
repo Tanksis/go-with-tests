5edad402518850795d032110e0fca80289fdc94c
Tanksis <18eek4@queensu.ca>
2024-07-03 17:23:12 -0400

    refactoring after hello world

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,15 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Chris") //reuse values
+	want := "Hello, Chris"
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want %q", got, want) //f strings, using .Errorf
 	}
 }
+
+//test files need to be in a file with name xxx_test.go
+//test function must start with word Test
+//test function takes one argument: t *testing.T
+//you need to import "testing"
